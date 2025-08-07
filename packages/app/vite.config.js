18add915fe0dfcf0c1d7e5704bcebcb619497183
Nether <vandeursenmart@gmail.com>
2025-08-07 01:56:36 -0700

    geminivercel

--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
   plugins: [react()],
   server: {
     proxy: {
